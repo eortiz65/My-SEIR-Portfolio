ffa8684c4fa77bf444bf4cc1d4684495f1029a26
Erwin Ortiz <113869285+eortiz65@users.noreply.github.com>
2022-12-01 15:20:15 +0000

    Initial commit test

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,10 +4,15 @@
  * Sample of a single responsibility component
  */
 
-import React from "react";
-
-const Header = () => {
-  return <h1>Educator React Codespaces JS Template</h1>;
-};
-
-export default Header;
+ import React from "react";
+ import PropTypes from "prop-types";
+ 
+ const Header = ({ title }) => {
+   return <h1>Educator React Codespaces JS Template - {title}</h1>;
+ };
+ 
+ Header.propTypes = {
+   title: PropTypes.string,
+ };
+ 
+ export default Header;
